1242a5dc11c9e7f4f718a4a929e5a70c328b229b
germanusK <germanuskeming@gmail.com>
2024-12-10 10:10:35 +0100

    print option added to stats

--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -1,29 +1,45 @@
 @extends('admin.layout')
 @section('section')
     <div class="py-4 container-fluid">
-        <table class="table">
-            <thead class="text-capitalize text-primary">
-                <th>@lang('text.sn')</th>
-                <th>@lang('text.word_campus')</th>
-                <th>@lang('text.word_count')</th>
-            </thead>
-            <tbody>
-                @foreach ($stats->groupBy('campus') as $campus => $campus_stats)
-                    <tr class="">
-                        <th class="text-dark h4 text-capitalize border border-black" colspan="3">{!! $campus !!}</th>
-                    </tr>
-                    @php
-                        $counter = 1;
-                    @endphp
-                    @foreach ($campus_stats as $stat_record)
-                        <tr>
-                            <td class="border-left">{{$counter++}}</td>
-                            <td class="border-left">{{$stat_record->program}}</td>
-                            <td class="border-left border-right">{{$stat_record->size}}</td>
+        <div class="d-flex justify-content-end py-3">
+            <button class="btn btn-primary btn-sm rounded h4 text-capitalize" onclick="printStats()">@lang('text.word_print')</button>
+        </div>
+        <div id="print-box">
+            <table class="table">
+                <thead class="text-capitalize text-primary">
+                    <th>@lang('text.sn')</th>
+                    <th>@lang('text.word_campus')</th>
+                    <th>@lang('text.word_count')</th>
+                </thead>
+                <tbody>
+                    @foreach ($stats->groupBy('campus') as $campus => $campus_stats)
+                        <tr class="">
+                            <th class="text-dark h4 text-capitalize border border-black" colspan="3">{!! $campus !!}</th>
                         </tr>
+                        @php
+                            $counter = 1;
+                        @endphp
+                        @foreach ($campus_stats as $stat_record)
+                            <tr>
+                                <td class="border-left">{{$counter++}}</td>
+                                <td class="border-left">{{$stat_record->program}}</td>
+                                <td class="border-left border-right">{{$stat_record->size}}</td>
+                            </tr>
+                        @endforeach
                     @endforeach
-                @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        let printStats = ()=>{
+            let printContent = $('#print-box');
+            let originalDoc = $(document.body).html();
+            $(document.body).html(printContent);
+            window.print();
+            $(document.body).html(originalDoc);
+        }
+    </script>
 @endsection
