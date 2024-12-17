7d5e75c35ce665596e5ab93c905f152b24606947
germanusK <germanuskeming@gmail.com>
2024-12-17 17:33:03 +0100

    ------

--- app/Http/Controllers/Admin/StatsController.php
+++ app/Http/Controllers/Admin/StatsController.php
@@ -3,21 +3,32 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\School;
 use App\Models\Students;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class StatsController extends Controller
 {
     //
 
     public function index(Request $request) {
+        $school_system_domain = School::first()->system_domain_url;
+        $endpoint = "{$school_system_domain}/api/program_admission_data";
+        $response = Http::get($endpoint)->collect('data');
+        // dd($response);
         $data['title'] = "Per Campus Per Program Statistics";
-        $data['stats'] = Students::whereNotNull('printed_at')
-            ->select([
-                'id', 'campus', 'program', DB::raw("COUNT(*) as size")
-            ])
-            ->groupBy(['campus', 'program'])->orderBy('program')->get();
+        $data['stats'] = $response->filter(function($rec){return $rec != null;})->map(function($rec){
+            return Students::whereNotNull('printed_at')->where(['program'=>$rec['name'], 'campus'=>$rec['campus']])->select(['id', 'campus', 'program', DB::raw("COUNT(*) as size")])->groupBy(['campus', 'program'])->get()
+                ->each(function($elm)use($rec){$elm->admitted_students = $rec['admitted_students'];});
+        });
+        // $data['stats'] = Students::whereNotNull('printed_at')
+        //     ->select([
+        //         'id', 'campus', 'program', DB::raw("COUNT(*) as size")
+        //     ])
+        //     ->groupBy(['campus', 'program'])->orderBy('program')->get();
+        dd($data);
         return view('admin.stats.index', $data);
     }
 }
--- resources/views/admin/stats/index.blade.php
+++ resources/views/admin/stats/index.blade.php
@@ -9,7 +9,8 @@
                 <thead class="text-capitalize text-primary">
                     <th>@lang('text.sn')</th>
                     <th>@lang('text.word_campus')</th>
-                    <th>@lang('text.word_count')</th>
+                    <th>@lang('text.word_admitted')</th>
+                    <th>@lang('text.word_printed')</th>
                 </thead>
                 <tbody>
                     @foreach ($stats->groupBy('campus') as $campus => $campus_stats)
@@ -23,6 +24,7 @@
                             <tr>
                                 <td class="border-left">{{$counter++}}</td>
                                 <td class="border-left">{{$stat_record->program}}</td>
+                                <td class="border-left border-right">{{$stat_record->admitted_students}}</td>
                                 <td class="border-left border-right">{{$stat_record->size}}</td>
                             </tr>
                         @endforeach
