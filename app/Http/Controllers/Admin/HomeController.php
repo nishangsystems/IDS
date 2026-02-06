17329780ac8e09c2b08e7779f76aab3eb629458f
germanusk <germanuskeming@gmail.com>
2026-02-06 13:35:56 +0100

    -----

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -259,10 +259,6 @@ class HomeController  extends Controller
 
     public function reprint_student_data(Request $request, $record_id) {
         $student = Students::find($record_id);
-        if($student->img_path != null and $student->photo != null){
-            $image_path = $student->img_path.'/'.$student->photo;
-            if(file_exists($image_path)){unlink($image_path);}
-        }
         $student->updated_at = null;
         $student->downloaded_at = null;
         $student->printed_at = null;
